a7de5ea78fe92d0d500d2e9183d88ad86280a2b1
Victoria <victoriabarbero88@gmail.com>
2020-10-28 22:18:51 +0100

    Simple web profile with moonjs

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const moon = new Moon({
+  el: ".app",
+  data: {
+    full_name: "Goodness Kayode | Pusher of Codes!",
+    email:"gtkbrain@gmail.com",
+    mobile_number:"+2341188469520",
+    short_info:"I am Goodness Kayode also called Pusher of Codes. Linguist turned engineer.Community lover"
+  }
+});
