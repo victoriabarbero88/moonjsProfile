222d425d4669023d4037eea12260b5e4493b8c12
Victoria <victoriabarbero88@gmail.com>
2020-10-29 15:40:32 +0100

    Some css changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 const moon = new Moon({
   el: ".app",
   data: {
-    full_name: "Goodness Kayode | Pusher of Codes!",
-    email:"gtkbrain@gmail.com",
-    mobile_number:"+2341188469520",
-    short_info:"I am Goodness Kayode also called Pusher of Codes. Linguist turned engineer.Community lover"
-  }
+    full_name: "Victoria Barbero",
+    email: "victoriabarbero88@gmail.com",
+    mobile_number: "+34644313940",
+    short_info:
+      "I'm a Junior Full Stack Developer, alway looking for new things to learn and improve",
+    img_: "../assets/vic.jpg",
+  },
 });
